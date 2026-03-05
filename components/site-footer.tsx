f8298d24e3be8365bf3424d6c23a8f74edc6def5
yeowool1010 <qyoong3579@gmail.com>
2026-03-05 23:32:17 +0900

    💄 [CSS] 무빈소장 구성품

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -28,14 +28,14 @@ export function SiteFooter() {
         <div className="mt-8 border-t border-stone-200 pt-8 text-center text-sm text-stone-500">
           <p className="font-medium text-stone-700">(주) 다움</p>
           <p className="mt-1">
-            대표이사 : 송슬옹 | 개인정보관리책임자 : 김소현
+            대표이사 : 박철진 | 개인정보관리책임자 : 박영광
           </p>
           <p className="mt-1">
-            주소 : 서울시 관악구 신림로 132, 1,2,3층 | 전화 문의: 1666-9784
+            주소 : 인천시 남동구 만수동 ~ | 전화 문의: 1234-1234
           </p>
-          <p className="mt-1">이메일 : contact@dawomfuneral.co.kr</p>
+          <p className="mt-1">이메일 : asdf@naver.com</p>
           <p className="mt-1">
-            사업자 등록번호 : 831-87-01971 | 통신판매업신고번호 : 2021-서울관악-2417
+            사업자 등록번호 : 000-00-00000 | 통신판매업신고번호 : 2026-인천남동-2417
           </p>
           <p className="mt-4 text-xs text-stone-400">
             © 2026. (주)다움 ALL RIGHTS RESERVED.
