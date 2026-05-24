f7068b046d2b7aa44ece791847212d9d38af060d
yeowool1010 <qyoong3579@gmail.com>
2026-05-25 01:18:11 +0900

    ✨ [Feat] 파비콘변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,10 @@ export const metadata: Metadata = {
   title: "숨김없는 장례의 시작, 다움",
   description:
     "막막한 상황, 다움이 함께 합니다. 장례식장, 장지 추천부터 상조 서비스, 장례 상담, 임종 접수까지. 숨김없는 장례의 시작.",
+  icons: {
+    icon: "/images/faviconnew.png",
+    apple: "/images/faviconnew.png",
+  },
 };
 
 const notoSansKR = Noto_Sans_KR({
